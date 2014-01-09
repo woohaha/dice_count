09c6d816154aca01e8a66dda1a009909df45c62f
woohaha <realwoohaha@gmail.com>
2014-01-09 23:30:20 +0800

    6 dots position detector v0.1

--- imporved_learn.py
+++ imporved_learn.py
@@ -1,20 +1,24 @@
 import cv2
-import math
-import numpy as np
 
-d_red = cv2.cv.RGB(150, 55, 65)
-l_red = cv2.cv.RGB(250, 200, 200)
-
-orig = cv2.imread("img/sample_6.jpg")
+orig = cv2.imread("img/sample_6_dash.jpg")
 img = orig.copy()
 img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 detector = cv2.FeatureDetector_create('SimpleBlob')
 fs = detector.detect(img2)
-fs.sort(key=lambda x: -x.size)
+# fs.sort(key=lambda x: -x.size)
 
-print(len(fs)) #dice dots
+print(len(fs))  # dice dots
 
 #TODO distinct the two position of 2, 3, 6 dots
+for points in fs:
+    print('(%d,%d)' % points.pt)
+
+p1x, p1y = fs[0].pt
+p2x, p2y = fs[1].pt
+if (p1y - p2y) < (p1x - p2x):
+    print('upright')
+else:
+    print('dash')
 
 
