283f05de7deef58a4201daa078b6f7db4b5646a6
woohaha <realwoohaha@gmail.com>
2014-07-25 00:35:14 +0800

    成功通過按鍵捕獲圖像並計算點數

--- imporved_learn.py
+++ imporved_learn.py
@@ -1,27 +1,30 @@
 # !/usr/bin/env python
 # coding=utf-8
+
 import cv2
-# import numpy as np
-import Parallel
+import numpy as np
+#import Parallel
+import math
 
 debug = True
+
 
 def sixposition(fs):
     p2x, p2y = fs[1].pt
     p3x, p3y = fs[2].pt
-    if p3x - p2x < 5:  # The "5" need to be fixed to meet the true environment
-        return '豎'
+    if p3x - p2x < 5:  # The "5" need to be fixed to meet the real environment
+        return True  # 縱向排列
     else:
-        return '橫'
+        return False  # 橫向排列
 
 
 def two_three_position(fs):
     p1x, p1y = fs[0].pt
     p3x, p3y = fs[-1].pt
     if (p3y - p1y) / (p3x - p1x) < 0:
-        return '反斜'
+        return True  # 反斜
     else:
-        return '斜槓'
+        return False  # 斜槓
 
 
 def position(fs):
@@ -33,31 +36,55 @@ def position(fs):
         return None
 
 
-def getimg():
-    with open('/dev/video0','rb')as cam:
-        img=cam.read(102400)
+def supress(x):
+    for f in fs:
+        distx = f.pt[0] - x.pt[0]
+        disty = f.pt[1] - x.pt[1]
+        dist = math.sqrt(distx * distx + disty * disty)
+        if (f.size > x.size) and (dist < f.size / 2):
+            return True
 
-    return img
-img = cv2.imread("webcam.png",cv2.CV_LOAD_IMAGE_GRAYSCALE)
-img2=cv2.bitwise_not(img)
-cv2.imshow('img2BW.jpg',img2)
-cv2.waitKey(0)
-#cap = cv2.VideoCapture()
-#ret, frame = cap.read()
-#cap.release()
-#buff=getimg()
-#frame_string=StringIO.StringIO(buff)
-#frame=cv2.imread(frame_string)
-#if not ret:
-#    raise Exception("Cannot Capture Video")
-#img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#img2 = rotateImage(img2, 90)
+# img = cv2.imread("webcam.png", cv2.CV_LOAD_IMAGE_GRAYSCALE) #讀取樣版圖
+# img2 = cv2.bitwise_not(img) #反色
 
+d_red = cv2.cv.RGB(150, 55, 65)
+l_red = cv2.cv.RGB(250, 200, 200)
+cap = cv2.VideoCapture(0)
 detector = cv2.FeatureDetector_create('SimpleBlob')
-fs = detector.detect(img2)
-fs.sort(key=lambda x: x.pt)
-print(len(fs), position(fs))
+ret, frame = cap.read()
+if not ret:
+    raise Exception("Cannot Capture Video")
 
-if debug:
-    for points in fs:
-        print(points.pt)
+h, w = frame.shape[:2]
+vis = np.zeros((h, w * 2 + 5), np.uint8)
+vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
+sfs = []
+
+while ret:
+    ret, frame = cap.read()
+    img = frame.copy()
+    img2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    vis[:h, :w] = frame
+
+    fs = detector.detect(img2)
+    fs.sort(key=lambda x: x.pt)
+    sfs = [x for x in fs if not supress(x)]
+    for f in sfs:
+        cv2.circle(img, (int(f.pt[0]), int(f.pt[1])), int(
+            f.size), d_red, 2, cv2.CV_AA)
+        cv2.circle(img, (int(f.pt[0]), int(f.pt[1])), int(
+            f.size), l_red, 1, cv2.CV_AA)
+    vis[:h, w + 5:w * 2 + 5] = img
+    if cv2.waitKey(0) & 0xFF == ord('c'):
+        for points in sfs:
+            print(points.pt)
+        if debug:
+            cv2.imwrite('captured.jpg', vis)
+
+    cv2.imshow('image', vis)
+    print(len(sfs), position(sfs))
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
